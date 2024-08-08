4fd8083ab8643ddc7e283dcada6417b6caa9ec49
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-08-07 23:04:10 -0400

    Added  logic to allow setting thread affinity (not work in macos)

--- utils/thread_util.h
+++ utils/thread_util.h
@@ -5,12 +5,16 @@
 #ifndef LOW_LATENCY_TRADING_APP_THREAD_UTIL_H
 #define LOW_LATENCY_TRADING_APP_THREAD_UTIL_H
 
+#include <chrono>
+#include <concepts>
+#include <functional>
 #include <iostream>
-#include <thread>
-#include <atomic>
-#include <unistd.h>
+#include <memory>
+#include <source_location>
+#include <string>
 #include <sys/syscall.h>
-
+#include <thread>
+#include <unistd.h>
 
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
@@ -22,10 +26,9 @@
 #include <sched.h>
 #endif
 
-
 namespace utils {
 
-inline auto set_thread_core_affinity(int core_id) -> bool{
+inline auto set_thread_core_affinity(int core_id) -> bool {
 
 #if defined(_WIN32) || defined(_WIN64)
     // Windows implementation
@@ -34,12 +37,12 @@ inline auto set_thread_core_affinity(int core_id) -> bool{
     return SetThreadAffinityMask(thread, mask) != 0;
 #elif defined(__APPLE__) && defined(__MACH__)
     // macOS implementation
-    thread_affinity_policy_data_t policy = { core_id };
+    thread_affinity_policy_data_t policy = {core_id};
     thread_port_t mach_thread = pthread_mach_thread_np(pthread_self());
-    return thread_policy_set(mach_thread,
-                             THREAD_AFFINITY_POLICY,
-                             (thread_policy_t)&policy,
-                             1) == KERN_SUCCESS;
+
+    kern_return_t result = thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY, (thread_policy_t) &policy, THREAD_AFFINITY_POLICY_COUNT);
+
+    return result == KERN_SUCCESS;
 #else
     // Linux implementation
     cpu_set_t cpuset;
@@ -49,10 +52,24 @@ inline auto set_thread_core_affinity(int core_id) -> bool{
 #endif
 }
 
-inline auto get_thread_core_affinity() noexcept -> std::jthread{
+// Concept to ensure the function is invocable with the given arguments
+template <typename F, typename... Args>
+concept Invocable = std::invocable<F, Args...>;
 
+template <typename F, typename... Args>
+    requires Invocable<F, Args...>
+[[nodiscard]] inline auto create_and_start_thread(int core_id, std::string_view name, F &&func, Args &&...args) noexcept -> std::jthread {
+    auto f = [core_id, name, function = std::forward<F>(func), ... arguments = std::forward<Args>(args)]() mutable {
+        if (core_id >= 0 && !set_thread_core_affinity(core_id)) {
+            std::cerr << "Failed to set core affinity for " << name << " " << std::this_thread::get_id() << " to " << core_id << '\n';
+            std::terminate();
+        }
+
+        std::cout << "Set core affinity for " << name << " " << std::this_thread::get_id() << " to " << core_id << '\n';
+
+        std::invoke(function, arguments...);
+    };
+    return std::jthread(f);
 }
-
-
-}
-#endif //LOW_LATENCY_TRADING_APP_THREAD_UTIL_H
+} // namespace utils
+#endif // LOW_LATENCY_TRADING_APP_THREAD_UTIL_H
