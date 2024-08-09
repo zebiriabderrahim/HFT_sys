94f6b5c5139967278f9821f2127346c4bf5b818b
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-08-08 22:57:47 -0400

    Added draft main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 
 #include "./utils/thread_util.h"
+#include "./utils/memory_pool.h"
 
 auto dummyFunction(int a, int b, bool sleep) {
     std::cout << "dummyFunction(" << a << "," << b << ")" << std::endl;
@@ -19,13 +20,41 @@ auto dummyFunction(int a, int b, bool sleep) {
     std::cout << "dummyFunction done." << std::endl;
 }
 
+//int main(int, char **) {
+//    using namespace utils;
+//
+//    auto t1 = createAndStartThread(-1, "dummyFunction1", dummyFunction, 12, 21, false);
+//    auto t2 = createAndStartThread(-1, "dummyFunction2", dummyFunction, 15, 51, true);
+//
+//    std::cout << "main exiting." << std::endl;
+//
+//    return 0;
+//}
+struct MyStruct {
+    int d_[3];
+};
+
 int main(int, char **) {
     using namespace utils;
 
-    auto t1 = create_and_start_thread(-1, "dummyFunction1", dummyFunction, 12, 21, false);
-    auto t2 = create_and_start_thread(-1, "dummyFunction2", dummyFunction, 15, 51, true);
+    MemoryPool<double> prim_pool(50);
+    MemoryPool<MyStruct> struct_pool(50);
 
-    std::cout << "main exiting." << std::endl;
+    for(auto i = 0; i < 50; ++i) {
+        auto p_ret = prim_pool.allocate(1);
+        auto s_ret = struct_pool.allocate(MyStruct{i, i+1, i+2});
+
+        std::cout << "prim elem:" << *p_ret << " allocated at:" << p_ret << std::endl;
+        std::cout << "struct elem:" << s_ret->d_[0] << "," << s_ret->d_[1] << "," << s_ret->d_[2] << " allocated at:" << s_ret << std::endl;
+
+        if(i % 5 == 0) {
+            std::cout << "deallocating prim elem:" << *p_ret << " from:" << p_ret << std::endl;
+            std::cout << "deallocating struct elem:" << s_ret->d_[0] << "," << s_ret->d_[1] << "," << s_ret->d_[2] << " from:" << s_ret << std::endl;
+
+            prim_pool.deallocate(p_ret);
+            struct_pool.deallocate(s_ret);
+        }
+    }
 
     return 0;
 }
