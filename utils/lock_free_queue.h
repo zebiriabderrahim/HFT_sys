dd9a4955027518ff93e44b3cdf07c9b96ce9536e
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-08-11 22:29:39 -0400

    Refactored the LFqueue draft

---
+++ utils/lock_free_queue.h
@@ -0,0 +1,64 @@
+//
+// Created by ABDERRAHIM ZEBIRI on 2024-08-10.
+//
+//
+// Created by ABDERRAHIM ZEBIRI on 2024-08-10.
+//
+
+#ifndef LOW_LATENCY_TRADING_APP_LOCK_FREE_QUEUE_H
+#define LOW_LATENCY_TRADING_APP_LOCK_FREE_QUEUE_H
+
+#include <atomic>
+#include <vector>
+#include <cstddef>
+
+#include "debug_assertion.h"
+
+
+namespace utils {
+
+template <typename T>
+class LFQueue {
+  public:
+    explicit LFQueue(std::size_t size)
+        : queue_(size){}
+
+    LFQueue() = delete;
+    LFQueue(const LFQueue&) = delete;
+    LFQueue(LFQueue&&) = delete;
+    LFQueue& operator=(const LFQueue&) = delete;
+    LFQueue& operator=(LFQueue&&) = delete;
+
+    [[nodiscard]] auto getNextToRead() const noexcept -> const T* {
+        return numElements_ > 0 ? &queue_[nextIndexToRead_] : nullptr;
+    }
+
+    [[nodiscard]] auto getNextToWrite() noexcept -> T* {
+        return numElements_ < queue_.size() ? &queue_[nextIndexToWrite_] : nullptr;
+    }
+
+    auto updateWriteIndex() noexcept -> void {
+        nextIndexToWrite_ = (nextIndexToWrite_ + 1) % queue_.size();
+        numElements_.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    auto updateReadIndex() noexcept -> void {
+        assertCondition(numElements_ > 0, "No elements to read.");
+        nextIndexToRead_ = (nextIndexToRead_ + 1) % queue_.size();
+        numElements_.fetch_sub(1, std::memory_order_relaxed);
+    }
+
+    [[nodiscard]] auto size() const noexcept -> std::size_t {
+        return numElements_.load(std::memory_order_relaxed);
+    }
+
+  private:
+    std::atomic<std::size_t> nextIndexToRead_{0};
+    std::atomic<std::size_t> nextIndexToWrite_{0};
+    std::atomic<std::size_t> numElements_{0};
+    std::vector<T> queue_;
+};
+
+} // namespace utils
+
+#endif // LOW_LATENCY_TRADING_APP_LOCK_FREE_QUEUE_H
