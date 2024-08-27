0e5c54bc65cd7c0ef3c690bb6602d0390cc09082
Abderrahim Zebiri <abderrahim.zebiri@polymtl.ca>
2024-08-27 19:21:16 -0400

    Updated the logger call std::aligned_storage_t instead of T directly in MemoryBlock. This allows us to allocate memory without constructing T objects.

--- utils/memory_pool.h
+++ utils/memory_pool.h
@@ -1,28 +1,26 @@
 //
 // Created by ABDERRAHIM ZEBIRI on 2024-08-08.
 //
-
 #ifndef LOW_LATENCY_TRADING_APP_MEMORY_POOL_H
 #define LOW_LATENCY_TRADING_APP_MEMORY_POOL_H
 
 #include <format>
 #include <vector>
-
+#include <memory>
 #include "debug_assertion.h"
 
 namespace utils {
-template <typename T> class MemoryPool {
-
+template <typename T>
+class MemoryPool {
   public:
-    explicit MemoryPool(std::size_t size) : memoryBlocks_(size, {T(), true}) {
-        assertCondition(reinterpret_cast<const MemoryBlock *>(&(memoryBlocks_[0].object)) == &(memoryBlocks_[0]),
-                        "T object should be first member of MemoryBlock.");
+    explicit MemoryPool(std::size_t size) : memoryBlocks_(size) {
+        ASSERT_CONDITION(reinterpret_cast<const MemoryBlock *>(&(memoryBlocks_[0].storage)) == &(memoryBlocks_[0]),
+                         "Storage should be first member of MemoryBlock.");
     }
 
     MemoryPool() = delete;
     MemoryPool(const MemoryPool &) = delete;
     MemoryPool(MemoryPool &&) = delete;
-
     MemoryPool &operator=(const MemoryPool &) = delete;
     MemoryPool &operator=(MemoryPool &&) = delete;
 
@@ -30,45 +28,44 @@ template <typename T> class MemoryPool {
     T *allocate(Args &&...args) {
         auto memoryBlock = &(memoryBlocks_[nextFreeIndex_]);
 
-        assertCondition(memoryBlock->isFree,
-                        std::format("Expected free MemoryBlock at index:{}", std::to_string(nextFreeIndex_)));
+        ASSERT_CONDITION(memoryBlock->isFree, "Expected free MemoryBlock at index:{}", std::to_string(nextFreeIndex_));
 
-        T *ret = &(memoryBlock->object);
-        ret = new (ret) T(std::forward<Args>(args)...); // placement new.
+        auto *ret = reinterpret_cast<T*>(&(memoryBlock->storage));
+        new (ret) T(std::forward<Args>(args)...); // placement new
         memoryBlocks_[nextFreeIndex_].isFree = false;
 
         updateNextFreeIndex();
-        return std::launder(ret);
+        return ret;
     }
 
-    void deallocate(const T* elem) noexcept {
+    void deallocate(T* elem) noexcept {
         const auto * elemPtr = reinterpret_cast<const MemoryBlock*>(elem);
         auto elemIndex = static_cast<std::size_t>(elemPtr - &(memoryBlocks_[0]));
 
-        assertCondition(elemIndex < memoryBlocks_.size(),"Invalid element index.");
-        assertCondition(!memoryBlocks_[elemIndex].isFree,
-                        std::format("Expected in-use MemoryBlock at index:{}", std::to_string(elemIndex)));
+        ASSERT_CONDITION(elemIndex < memoryBlocks_.size(), "Invalid element index.");
+        ASSERT_CONDITION(!memoryBlocks_[elemIndex].isFree, "Expected in-use MemoryBlock at index:{}", std::to_string(elemIndex));
 
+        elem->~T(); // Call destructor
         memoryBlocks_[elemIndex].isFree = true;
     }
 
-
   private:
-     void updateNextFreeIndex() noexcept {
+    void updateNextFreeIndex() noexcept {
         const auto initialFreeIndex = nextFreeIndex_;
-        while (!memoryBlocks_[nextFreeIndex_].isFree) {
+        do {
             ++nextFreeIndex_;
-            if (nextFreeIndex_ == memoryBlocks_.size()) [[unlikely]] {
+            if (nextFreeIndex_ == memoryBlocks_.size()) {
                 nextFreeIndex_ = 0;
             }
-            if (initialFreeIndex == nextFreeIndex_ ) [[unlikely]] {
-                assertCondition(initialFreeIndex != nextFreeIndex_, "Memory Pool out of space.");
+            if (memoryBlocks_[nextFreeIndex_].isFree) {
+                return;
             }
-        }
+        } while (initialFreeIndex != nextFreeIndex_);
+        ASSERT_CONDITION(false, "Memory Pool out of space.");
     }
 
     struct MemoryBlock {
-        T object;
+        std::aligned_storage_t<sizeof(T), alignof(T)> storage;
         bool isFree = true;
     };
 
